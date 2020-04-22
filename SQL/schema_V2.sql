5d0a5b413e20a4ff6b564a351bb9b00a62910841
Benjamin Steele <bsteele146@gmail.com>
2020-04-21 18:20:14 -0700

    Should now be able to insert schedules into the database through the add events page. Changed a little bit of SQL to make things work better. Haven't really bug checked through.

--- SQL/schema_V2.sql
+++ SQL/schema_V2.sql
@@ -387,10 +387,7 @@ JOIN Events AS e ON s.eventID=e.eventID
 LEFT JOIN Sensors AS n on s.sensorID=n.sensorID
 JOIN Outputs AS o on s.outputID=o.outputID
 WHERE s.Senabled = 1
-  AND (
-    (LOCALTIMESTAMP BETWEEN s.scheduleStartDate AND s.scheduleStopDate)
-    OR (LOCALTIMESTAMP <= s.scheduleStopDate AND s.scheduleStartDate IS NULL)
-    OR s.scheduleStopDate IS NULL)
+  AND ((LOCALTIMESTAMP <= s.scheduleStopDate OR s.scheduleStopDate IS NULL) AND (LOCALTIMESTAMP >= s.scheduleStartDate OR s.scheduleStartDate IS NULL))
 ORDER BY -eventTriggerTime DESC, outputName DESC
 $$
 DELIMITER ;
