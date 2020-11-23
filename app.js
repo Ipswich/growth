e8942ec2f2560fc74c15fe57aaae9d237e85126e
Benjamin Steele <bsteele146@gmail.com>
2020-11-22 18:00:26 -0800

    Removed debug printouts from app.js for routes ;)

--- app.js
+++ app.js
@@ -42,8 +42,7 @@ app.set('config', config);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
-// QUIET YE PRINTOUTS
-app.use(logger('dev')); 
+// app.use(logger('dev')); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
