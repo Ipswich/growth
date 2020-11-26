c18a58acb8d4aed4c109b73098046b4010de0d4c
Benjamin Steele <bsteele146@gmail.com>
2020-11-26 00:39:03 -0800

    Reworked the timing/rerunning of events. They now use a moment object to keep track of whether or not they should be run again. Once the minute of the current time is the same as the stored moment, the event is removed from the list. If the scheduleID isn't in the list, it's able to be run. Warn should theoretically be fully functional now. Gonna poke around a bit with the interface end and see how it goes.

--- routes/addTimeEvent.js
+++ routes/addTimeEvent.js
@@ -16,7 +16,6 @@ router.post('/', auth, async function(req, res, next) {
   }
   let output = sanitizedData.TimeOutput.slice(1, -1).split("|")[0]
   let event = sanitizedData.TimeEvent.slice(1, -1).split("|")[0]
-  console.log(sanitizedData)
   //DO STUFF WITH ESCAPED DATA
   await dbcalls.addNewSchedule("'Time'", event, null, null, output, sanitizedData.TimeOutputValue, null, "'" + utils.formatTimeStringForDB(sanitizedData.TimeTrigger) + "'", null, sanitizedData.TimeWarnInterval, utils.formatDateString(sanitizedData.TimeStartDate), utils.formatDateString(sanitizedData.TimeEndDate), '1', "'"+res.locals.username+"'", null)
   .catch(() => {
