988b09a676473ef18e8383d89ba7d34c483adad0
Benjamin Steele <bsteele146@gmail.com>
2020-12-18 23:53:06 -0800

    Added more to wiki, modified example_setup.sql to be more descriptive.

--- SQL/example_setup.sql
+++ SQL/example_setup.sql
@@ -1,15 +1,20 @@
+-- Available Sensor Types: Temperature, Pressure, Humidity
+-- Available Output Types: Light, PWM Light, Exhaust, PWM Exhaust, Intake, PWM intake, Circulation, Heat, Water
 
-CALL addNewUser ('admin', '$2b$10$KDSExpDtZd1HXxCZemfxxeZlRCqXD1ZhDPNkIBxjeaYZyaxMUVi9m', 'bsteele146@gmail.com');
+-- Add a user
+CALL addNewUser ('admin', '$2b$10$KDSExpDtZd1HXxCZemfxxeZlRCqXD1ZhDPNkIBxjeaYZyaxMUVi9m', NULL);
 
-CALL addNewOutput('Light', 'QB Growlight 1', 1, 1, '');
-CALL addNewOutput('Light', 'QB Growlight 2', 1, 1,  '');
-CALL addNewOutput('Light', '660nm Growlight', 1, 1,  '');
-CALL addNewOutput('Water', 'Water Valve', 0, 0, '');
-CALL addNewOutput('Heat', 'Heater', 0, 0, '');
-CALL addNewOutput('Exhaust', 'Exhaust Fan', 0, 0, '');
-CALL addNewOutput('Circulation', 'Circulation Fan 1', 0, 0, '');
-CALL addNewOutput('Circulation', 'Circulation Fan 2', 0, 0, '');
+-- Add outputs
+CALL addNewOutput('PWM Light', 'QB Growlight 1', '');
+CALL addNewOutput('PWM Light', 'QB Growlight 2',  '');
+CALL addNewOutput('PWM Light', '660nm Growlight',  '');
+CALL addNewOutput('Water', 'Water Valve', '');
+CALL addNewOutput('Heat', 'Heater', '');
+CALL addNewOutput('PWM Exhaust', 'Exhaust Fan', '');
+CALL addNewOutput('Circulation', 'Circulation Fan 1', '');
+CALL addNewOutput('Circulation', 'Circulation Fan 2', '');
 
+-- Add sensors 
 CALL addNewSensor('BME280', 'Temperature', 'Control Box', '°F', 1, 'I2C');
 CALL addNewSensor('BME280', 'Pressure', 'Control Box', 'kPa', 1, 'I2C');
 CALL addNewSensor('BME280', 'Humidity', 'Control Box', '% rH', 1, 'I2C');
