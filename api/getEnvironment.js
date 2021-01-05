b2cfcd91547f0d9a5279d5bbd38bf5b07c1926e2
Benjamin Steele <bsteele146@gmail.com>
2021-01-05 01:29:17 -0800

    Fixed a bug. When the server is loading, it does not have chart data generated yet. This properly handles the scary message from unhandled promises.

--- api/getEnvironment.js
+++ api/getEnvironment.js
@@ -6,23 +6,25 @@ router.get('/', async function(req, res, next) {
     var indexData = await utils.getIndexData(res, req).catch(() => {
         res.status(500).send("Database error, could not fetch environment data.")
     })
-    let returnData = {}
-    returnData.msg = "Sensor event successfully added!"
-    returnData.schedules = indexData.schedules
-    returnData.currentConditions = indexData.currentConditions
-    res.status(200).send(returnData);  
+    if(!res.headersSent){
+        let returnData = {}
+        returnData.schedules = indexData.schedules
+        returnData.currentConditions = indexData.currentConditions
+        res.status(200).send(returnData);  
+    }
 })
 
 router.post('/', async function(req, res, next) {
     var indexData = await utils.getIndexData(res, req, req.body.interval).catch(() => {
-        res.status(500).send("Database error, could not fetch environment data.")
+        return res.status(500).send("Database error, could not fetch environment data.")
     })
-    let returnData = {}
-    returnData.msg = "Sensor event successfully added!"
-    returnData.schedules = indexData.schedules
-    returnData.currentConditions = indexData.currentConditions
-    returnData.authentication = indexData.authentication
-    res.status(200).send(returnData);  
+    if(!res.headersSent){
+        let returnData = {}
+        returnData.schedules = indexData.schedules
+        returnData.currentConditions = indexData.currentConditions
+        returnData.authentication = indexData.authentication
+        res.status(200).send(returnData);      
+    }
 })
 
 module.exports = router;
