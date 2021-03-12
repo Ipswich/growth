34f78fe2e3ad74d524d61c290f24643ae92f996a
Benjamin Steele <bsteele146@gmail.com>
2021-03-12 00:45:35 -0800

    Updated node.js.yml

--- test/setup.spec.js
+++ test/setup.spec.js
@@ -3,7 +3,6 @@ var app
 const dbcalls = require('../custom_node_modules/utility_modules/database_calls.js')
 
 before(async (done) => {
-  console.log(process.env.DB_PORT)
   await dbcalls.createTestDBStructure().then(() => {
     app = require("../app.js");
     app.once('started', () => {
