e5e6c612bdaacb3d84ed322c6e34944e07d26a30
Ipswich <Ipswich@users.noreply.github.com>
2021-10-27 14:14:05 -0700

    Commented out most of the old tests. Reviewed and wrote tests for most of the config initialization code. Things seem to be working. Going to keep moving forward on writing more tests and stuff. Onward! (#88)

--- test/setup.js
+++ test/setup.js
@@ -1,11 +1,11 @@
 
 const sinon = require('sinon')
-const dbcalls = require('../custom_node_modules/utility_modules/database_calls.js')
+// const dbcalls = require('../custom_node_modules/utility_modules/database_calls.js')
 var app;
 
 exports.mochaGlobalSetup = async function() {
-    await dbcalls.createTestDBStructure()
-    await dbcalls.addTestDBData()
+    // await dbcalls.createTestDBStructure()
+    // await dbcalls.addTestDBData()
     // await new Promise(function(resolve, reject) {
     //   app = require("../app.js");
     //   app.once('started', resolve);
