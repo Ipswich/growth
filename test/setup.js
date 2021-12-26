4653e27cc6c730653a8395d23d155bea96d9b0a2
Ipswich <Ipswich@users.noreply.github.com>
2021-12-26 14:30:06 -0800

    Renamed Mappings.js to mappings.js. Extracted pin counting to a function in mappings.js to avoid repetition. Cleaned out some old commented-out code in setup.js.

--- test/setup.js
+++ test/setup.js
@@ -1,15 +1,8 @@
 
 const sinon = require('sinon')
-// const dbcalls = require('../custom_node_modules/utility_modules/database_calls.js')
 var app;
 
 exports.mochaGlobalSetup = async function() {
-    // await dbcalls.createTestDBStructure()
-    // await dbcalls.addTestDBData()
-    // await new Promise(function(resolve, reject) {
-    //   app = require("../app.js");
-    //   app.once('started', resolve);
-    // })
 }
 
 exports.mochaGlobalTeardown = async function() {
