6a81560fb549457313cf2cb8ba0a911054000eaf
Ipswich <Ipswich@users.noreply.github.com>
2022-03-25 00:25:10 -0700

    Fixed a mislabled property in getLatestImageName(), and the test. Also modified the images route, but it seems to work as intended now. Now just to incorporate the image into the actual webpage rather than just the API.

--- api/images.js
+++ api/images.js
@@ -8,7 +8,7 @@ let config = config_helper.getConfig()
 router.get('/latest', async function(req, res, next) {
   try {
     let image_name = await utils.getLatestImageName()
-    res.status(200).sendFile(config.camera.image_directory + '/' + image_name)
+    res.status(200).sendFile(image_name, {root: config.camera.image_directory})
   } catch (e) {
     res.status(500).send(e.message)
   }
