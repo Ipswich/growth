baf8590758bc2e461edc849333d9b6d430690a29
Ipswich <Ipswich@users.noreply.github.com>
2022-04-17 16:39:52 -0700

    Fixed a bug and updated a function that was supposed to get the latest image. Turns out it grabbed any file, so now it only grabs images AND from specified directory. Updated tests and stuff, should work as intended now. (#103)

--- api/images.js
+++ api/images.js
@@ -9,7 +9,7 @@ let config = config_helper.getConfig()
 
 router.get('/latest', auth, async function(req, res, next) {
   try {
-    let image_name = await utils.getLatestImageName()
+    let image_name = await utils.getLatestFileName('jpg', config.camera.image_directory)
     res.status(200).sendFile(image_name, {root: config.camera.image_directory})
   } catch (e) {
     res.status(500).send(e.message)
