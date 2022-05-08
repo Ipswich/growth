a0237a15ed2f6f786f7dfb86f8445e8bb3ca24b2
Ben Steele <bsteele146@gmail.com>
2022-05-08 16:16:33 -0700

    Added api routes for outputs, sensors, events, and warnstate. Basically everything in the app state object that isn't part of johnny five, all the information but none of the functional bits.

--- api/state.js
+++ api/state.js
@@ -3,10 +3,56 @@ const router = express.Router();
 const auth = require('../middleware/authenticateLogin.js')
 const printouts = require('../custom_node_modules/utility_modules/printouts')
 
-router.get('/current', auth, async function(req, res, next){
-  let return_data = req.app.get('state')
-  console.log(return_data)
-  res.status(200).send(return_data)
+router.get('/outputs', auth, async function(req, res, next){
+  try {
+    let state = req.app.get('state')
+    return_data = {}
+    return_data.outputState = state.outputState.getOutputData()
+    res.status(200).send(return_data)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.get('/sensors', auth, async function(req, res, next){
+  try {
+    let state = req.app.get('state')
+    return_data = {}
+    return_data.sensorState = state.sensorState.getSensorData()
+    res.status(200).send(return_data)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.get('/events', auth, async function(req, res, next){
+  try {
+    let state = req.app.get('state')
+    return_data = {}
+    return_data.events = state.events
+    for(data in return_data.events){
+      if (data.Eenabled == 1){
+        data.Eenabled = true
+      } else {
+        data.Eenabled = false
+      }
+    }
+
+    res.status(200).send(return_data)
+  } catch (e) {
+    res.status(500).send()
+  }
+})
+
+router.get('/warn_state', auth, async function (req, res, next) {
+  try {
+    let state = req.app.get('state')
+    return_data = {}
+    return_data.warn_state = state.warnState
+    res.status(200).send(return_data)
+  } catch (e) {
+    res.status(500).send()
+  }
 })
 
 module.exports = router;
