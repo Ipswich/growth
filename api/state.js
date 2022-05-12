5a683cfde8dfc31bfd65dd099ec6a93d567fdfc0
Ben Steele <bsteele146@gmail.com>
2022-05-11 17:36:27 -0700

    Cleand up and fixed a few routes, modified functions in both Outputstate and Sensorstate to return individual states with an optional parameter.

--- api/state.js
+++ api/state.js
@@ -3,11 +3,11 @@ const router = express.Router();
 const auth = require('../middleware/authenticateLogin.js')
 const printouts = require('../custom_node_modules/utility_modules/printouts')
 
+// Output states
 router.get('/outputs', auth, async function(req, res, next){
   try {
     let state = req.app.get('state')
-    return_data = {}
-    return_data.outputState = state.outputState.getOutputData()
+    let return_data = state.outputState.getOutputData()
     res.status(200).send(return_data)
   } catch (e) {
     res.status(500).send()
@@ -17,23 +17,23 @@ router.get('/outputs', auth, async function(req, res, next){
 router.get('/outputs/:outputid', auth, async function(req, res, next){
   try {
     let state = req.app.get('state')
-    let outputID = parseInt(req.body.outputid)
+    let outputID = parseInt(req.params.outputid)
     if (outputID == NaN){
       throw new TypeError()
     }
-    return_data = {}
-    return_data.outputState = state.outputState.getOutput(outputID)
+    let return_data = state.outputState.getOutputData(outputID).pop()
     res.status(200).send(return_data)
   } catch (e) {
     res.status(500).send()
   }
 })
 
+// Sensor states
 router.get('/sensors', auth, async function(req, res, next){
   try {
     let state = req.app.get('state')
-    return_data = {}
-    return_data.sensorState = state.sensorState.getSensorData()
+    let return_data = {}
+    return_data = state.sensorState.getSensorData()
     res.status(200).send(return_data)
   } catch (e) {
     res.status(500).send()
@@ -43,22 +43,22 @@ router.get('/sensors', auth, async function(req, res, next){
 router.get('/sensors/:sensorid', auth, async function(req, res, next){
   try {
     let state = req.app.get('state')
-    let sensorID = parseInt(req.body.sensorid)
+    let sensorID = parseInt(req.params.sensorid)
     if (sensorID == NaN){
       throw new TypeError()
     }
-    return_data = {}
-    return_data.sensorState = state.sensorState.getSensor(sensorID)
+    let return_data = state.sensorState.getSensorData(sensorID).pop()
     res.status(200).send(return_data)
   } catch (e) {
     res.status(500).send()
   }
 })
 
+// Events
 router.get('/events', auth, async function(req, res, next){
   try {
     let state = req.app.get('state')
-    return_data = {}
+    let return_data = {}
     return_data.events = state.events
     for(data in return_data.events){
       if (data.Eenabled == 1){
@@ -73,10 +73,11 @@ router.get('/events', auth, async function(req, res, next){
   }
 })
 
+// Email Warn State
 router.get('/warn_state', auth, async function (req, res, next) {
   try {
     let state = req.app.get('state')
-    return_data = {}
+    let return_data = {}
     return_data.warn_state = state.warnState
     res.status(200).send(return_data)
   } catch (e) {
